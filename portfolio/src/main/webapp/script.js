9d4d7bbceb3311f811874b87734c9d7ca0a76c66
Kozlova Olga <grenlayk@google.com>
2020-07-31 13:21:13 +0000

    Create map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -39,3 +39,10 @@ function createMyElement(text, type) {
   element.innerText = text;
   return element;
 }
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
